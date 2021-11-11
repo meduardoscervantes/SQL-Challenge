1afcbf4d13bcd099113bd00e54cbef10300c6ee6
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-10 20:32:26 -0600

    updated the table schema

--- EmoloyeeSQL/table_schema.sql
+++ EmoloyeeSQL/table_schema.sql
@@ -1,47 +1,34 @@
 CREATE TABLE departments(
-	id SERIAL,
 	dept_no VARCHAR(32),
-	dept_name VARCHAR(32),
-	PRIMARY KEY (id)
+	dept_name VARCHAR(32)
 );
 
 CREATE TABLE dept_emp(
-	id SERIAL,
 	emp_no int,
-	dept_no VARCHAR(32),
-	PRIMARY KEY (id)
+	dept_no VARCHAR(32)
 );
 
 CREATE TABLE dept_manager(
-	id SERIAL,
 	dept_no VARCHAR(32),
-	emp_no int,
-	PRIMARY KEY (id)
+	emp_no int
 );
 
 CREATE TABLE employees(
-	id SERIAL,
 	emp_no int,
 	emp_title VARCHAR(32),
 	birth_date DATE,
 	first_name VARCHAR(32),
 	last_name VARCHAR(32),
 	sex VARCHAR(32),
-	hite_date DATE,
-	PRIMARY KEY (id)
+	hite_date DATE
 );
 
 CREATE TABLE salaries(
-	id SERIAL,
 	emp_no int,
-	salary int,
-	PRIMARY KEY (id)
-
+	salary int
 );
 
 CREATE TABLE titles(
-	id SERIAL,
 	title_id VARCHAR(32),
-	title VARCHAR(32),
-	PRIMARY KEY (id)
+	title VARCHAR(32)
 );
