fd63281490f092cd6a2a9558003470afe7d00135
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-13 13:53:07 -0600

    Updated the schema to change the DATE data type to VARCHAR

--- EmoloyeeSQL/table_schema.sql
+++ EmoloyeeSQL/table_schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE employees(
 	emp_no INT PRIMARY KEY,
 	emp_title VARCHAR(32),
 	FOREIGN KEY (emp_title) REFERENCES titles(title_id),
-	birth_date DATE,
+	birth_date VARCHAR(32),
 	first_name VARCHAR(32),
 	last_name VARCHAR(32),
 	sex VARCHAR(32),
@@ -20,8 +20,9 @@ CREATE TABLE employees(
 );
 
 CREATE TABLE dept_emp(
-	emp_no INT PRIMARY KEY,
+	emp_no INT,
 	dept_no VARCHAR(32),
+	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
