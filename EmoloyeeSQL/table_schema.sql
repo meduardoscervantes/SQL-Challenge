a692d62724c0d5dbc5c0bd4bc09500c4bfc5316d
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-13 12:58:20 -0600

    Updated the schema to import all of the tables

--- EmoloyeeSQL/table_schema.sql
+++ EmoloyeeSQL/table_schema.sql
@@ -1,23 +1,17 @@
 CREATE TABLE departments(
-	dept_no VARCHAR(32),
-	dept_name VARCHAR(32),
-	PRIMARY KEY (dept_no),
-	UNIQUE(dept_name)
+	dept_no VARCHAR(32) PRIMARY KEY,
+	dept_name VARCHAR(32)
 );
 
-CREATE TABLE dept_emp(
-	emp_no int,
-	dept_no VARCHAR(32)
-);
-
-CREATE TABLE dept_manager(
-	dept_no VARCHAR(32),
-	emp_no int
+CREATE TABLE titles(
+	title_id VARCHAR(32) PRIMARY KEY,
+	title VARCHAR(32)
 );
 
 CREATE TABLE employees(
-	emp_no int,
+	emp_no INT PRIMARY KEY,
 	emp_title VARCHAR(32),
+	FOREIGN KEY (emp_title) REFERENCES titles(title_id),
 	birth_date DATE,
 	first_name VARCHAR(32),
 	last_name VARCHAR(32),
@@ -25,12 +19,23 @@ CREATE TABLE employees(
 	hire_date DATE
 );
 
-CREATE TABLE salaries(
-	emp_no int,
-	salary int
+CREATE TABLE dept_emp(
+	emp_no INT PRIMARY KEY,
+	dept_no VARCHAR(32),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
-CREATE TABLE titles(
-	title_id VARCHAR(32),
-	title VARCHAR(32)
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(32),
+	emp_no INT,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY,
+	salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
