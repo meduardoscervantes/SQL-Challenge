aa7f1e4e41be8ca83a193d1d25c66ad76f684b92
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-13 22:21:52 -0600

    Added in the query for the #3 question

--- EmoloyeeSQL/table_query.sql
+++ EmoloyeeSQL/table_query.sql
@@ -14,7 +14,14 @@ WHERE
 hire_date LIKE '%1986';
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
+SELECT
+dept_manager.dept_no, 
+departments.dept_name,
+dept_manager.emp_no,
+employees.last_name, employees.first_name
+FROM dept_manager
+INNER JOIN departments ON dept_manager.dept_no=departments.dept_no
+INNER JOIN employees ON dept_manager.emp_no=employees.emp_no
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
 
