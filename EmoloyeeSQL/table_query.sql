91c8bc4e521074784eed7ffc4f0b61bd13af942c
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-15 17:43:28 -0600

    updated the answer for question #7

--- EmoloyeeSQL/table_query.sql
+++ EmoloyeeSQL/table_query.sql
@@ -45,8 +45,18 @@ INNER JOIN employees ON dept_emp.emp_no=employees.emp_no
 INNER JOIN departments ON dept_emp.dept_no=departments.dept_no
 WHERE
 dept_emp.dept_no = 'd007'
---List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT
+dept_emp.emp_no, 
+employees.last_name, employees.first_name, 
+departments.dept_name
+FROM 
+dept_emp
+INNER JOIN employees ON dept_emp.emp_no=employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no=departments.dept_no
+WHERE
+dept_emp.dept_no = 'd007' OR dept_emp.dept_no = 'd005'
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
