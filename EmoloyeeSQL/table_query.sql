6ee038e96004d2c6f5f4a20405c479306ebfddcb
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-15 17:41:05 -0600

    updated the answer for q#6

--- EmoloyeeSQL/table_query.sql
+++ EmoloyeeSQL/table_query.sql
@@ -35,8 +35,16 @@ WHERE
 first_name = 'Hercules' AND last_name LIKE 'B%'
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
-
+SELECT
+dept_emp.emp_no, 
+employees.last_name, employees.first_name, 
+departments.dept_name
+FROM 
+dept_emp
+INNER JOIN employees ON dept_emp.emp_no=employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no=departments.dept_no
+WHERE
+dept_emp.dept_no = 'd007'
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 
