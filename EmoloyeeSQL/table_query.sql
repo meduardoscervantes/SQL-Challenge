106e2af599169ffbb8aa39e775ffd4e9549d8edb
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-15 18:08:14 -0600

    updated the answer with issues

--- EmoloyeeSQL/table_query.sql
+++ EmoloyeeSQL/table_query.sql
@@ -24,7 +24,13 @@ INNER JOIN departments ON dept_manager.dept_no=departments.dept_no
 INNER JOIN employees ON dept_manager.emp_no=employees.emp_no;
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT
+employees.emp_no, employees.last_name, employees.first_name,
+departments.dept_name
+FROM
+employees
+INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON dept_emp.dept_no=departments.dept_no
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT 
